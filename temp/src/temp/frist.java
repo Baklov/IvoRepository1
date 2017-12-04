34c40a2589314f5db4cef1b5493c65fccc30cbc8
Ivo <baklov@abv.bg>
2017-12-04 18:40:50 +0200

    init

---
+++ temp/src/temp/frist.java
@@ -0,0 +1,17 @@
+package temp;
+
+import java.awt.SystemColor;
+
+public class frist {
+
+	public frist() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("I am hera!");
+
+	}
+
+}
